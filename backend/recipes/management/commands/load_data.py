e88ad69ce3b8c3310622ae140ca9a630620b6227
Anton Sidorov <ursmoscpw@yandex.ru>
2023-11-29 23:36:51 +0300

    load_data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -10,8 +10,16 @@ class Command(BaseCommand):
         with open('ingredients.json', 'rb') as f:
             data = json.load(f)
             for i in data:
-                ingredient = Ingredient()
-                ingredient.name = i['name']
-                ingredient.measurement_unit = i['measurement_unit']
-                ingredient.save()
-                print(i['name'], i['measurement_unit'])
+                ingredient_name = i['name']
+                measurement_unit = i['measurement_unit']
+
+                existing_ingredient = Ingredient.objects.filter(name=ingredient_name).first()
+
+                if existing_ingredient:
+                    existing_ingredient.measurement_unit = measurement_unit
+                    existing_ingredient.save()
+                    print(f"Updated: {ingredient_name} - {measurement_unit}")
+                else:
+                    ingredient = Ingredient(name=ingredient_name, measurement_unit=measurement_unit)
+                    ingredient.save()
+                    print(f"Inserted: {ingredient_name} - {measurement_unit}")
