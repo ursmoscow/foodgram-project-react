db19817b4895ef07be2a328665a55dd4eee05e68
Anton Sidorov <ursmoscpw@yandex.ru>
2023-11-30 02:00:23 +0300

    RELEASE

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -13,13 +13,17 @@ class Command(BaseCommand):
                 ingredient_name = i['name']
                 measurement_unit = i['measurement_unit']
 
-                existing_ingredient = Ingredient.objects.filter(name=ingredient_name).first()
+                existing_ingredient = Ingredient.objects.filter(
+                    name=ingredient_name
+                ).first()
 
                 if existing_ingredient:
                     existing_ingredient.measurement_unit = measurement_unit
                     existing_ingredient.save()
                     print(f"Updated: {ingredient_name} - {measurement_unit}")
                 else:
-                    ingredient = Ingredient(name=ingredient_name, measurement_unit=measurement_unit)
+                    ingredient = Ingredient(
+                        name=ingredient_name, measurement_unit=measurement_unit
+                    )
                     ingredient.save()
                     print(f"Inserted: {ingredient_name} - {measurement_unit}")
